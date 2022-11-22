320b258a9c57658ae295561b4d95d831ea3ae354
Elvis Vukesu <elvisvukesu@icloud.com>
2022-11-22 00:11:14 -0800

    vars added

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "aws_access_key" {
+  description = "enter the value for the aws access key"
+}
+
+variable "aws_secret_access_key" {
+  description = "enter the value of the aws secret access key"
+}
+
