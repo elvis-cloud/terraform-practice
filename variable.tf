fa32769f36b69379a46b2ffde2038825cdcd819b
Elvis Vukesu <elvisvukesu@icloud.com>
2023-01-05 00:19:02 -0800

    Description modified

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "aws_access_key" {
-  description = "enter the value for the aws access key"
+  description = "aws access key stored in terraform cloud"
 }
 
 variable "aws_secret_access_key" {
-  description = "enter the value of the aws secret access key"
+  description = "aws secret access key stored in terraform cloud"
 }
 
